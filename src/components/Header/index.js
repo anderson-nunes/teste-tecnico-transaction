ac1dee0e7ab9539674301e01d092c0ccfa74b78f
anderson-nunes <andersonribeironunes@hotmail.com>
2023-11-01 09:24:17 -0300

    Restilizado o header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,6 +5,11 @@ export const Header = () => {
   return (
     <S.ContainerHeader>
       <h1>Logo</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </S.ContainerHeader>
   )
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,11 +1,30 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const ContainerHeader = styled.header`
-
   width: 100%;
   height: 150px;
   background-color: #d1d1d1;
-  align-items: center;
-  justify-content: center;
   display: flex;
-`
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 50px; 
+
+
+  ul {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    gap: 20px; /* Espaçamento entre os itens da lista. */
+
+    li {
+      margin-right: 1rem;
+      font-size: 1.5rem;
+      cursor: pointer;
+      transition: color 0.3s;
+
+      &:hover {
+        color: #363636;
+      }
+    }   
+  }
+`;
